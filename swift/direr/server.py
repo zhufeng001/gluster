79ee9e3564927a31f7e9f71582cc139e7af1bd98
root <sun__shines@163.com>
2015-04-17 14:57:48 +0800

    dir recycle

--- swift/direr/server.py
+++ swift/direr/server.py
@@ -5,11 +5,17 @@ import gluster.swift.common.constraints
 from swift.direr import server
 from gluster.swift.common.DiskDirer import DiskDirer
 
+from gluster.swift.common.DirMeta import DirMeta
 
 class DirerController(server.DirerController):
     def _get_direr_broker(self, drive, part, account, container,direr):
         
         return DiskDirer(self.root, drive, account, container, direr,self.logger)
+
+
+    def _get_meta_broker(self, drive, part, account, container,direr,recycle_uuid):
+        
+        return DirMeta(self.root, drive, account, container, direr,self.logger,recycle_uuid=recycle_uuid)
 
 
 # def app_factory(global_conf, **local_conf):
